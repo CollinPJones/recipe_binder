80aa715b7137d8084fc9567daeb499f1cc1236f7
Collin  Jones <collinpjones@users.noreply.github.com>
2016-11-22 19:54:17 -0600

    Created Measurement to Measurement Unit Associations

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,3 +1,6 @@
 class Measurement < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+
+  #Measurement to Measurement Unit Association
+  has_many :units, class_name: "MeasurementUnit", foreign_key: "measurement_id"
 end
--- app/models/measurement_unit.rb
+++ app/models/measurement_unit.rb
@@ -1,4 +1,8 @@
 class MeasurementUnit < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :measurement_id, presence:true
+
+  #Measurement Unit to Measurement Association
+  belongs_to :measurement
+
 end
