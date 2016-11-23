ac8dc24ee4654579586186672139ba7cd8d8d8b1
Collin  Jones <collinpjones@users.noreply.github.com>
2016-11-22 20:05:59 -0600

    Created Ingredient Validations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,7 @@
 class Ingredient < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :purchase_container, presence: true
+  validates :purchase_quanitity, presence: true
+  validates :category_id, presence: true
+  validates :purchase_measurement_unit_id, presence: true
 end
