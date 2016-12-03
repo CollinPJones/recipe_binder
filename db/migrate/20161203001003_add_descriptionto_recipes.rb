55d1ec34a029941cff7756995d8fc928e1f4ac84
Collin  Jones <collinpjones@users.noreply.github.com>
2016-12-02 18:12:28 -0600

    Added "Description" Column to Recipe DB

---
+++ db/migrate/20161203001003_add_descriptionto_recipes.rb
@@ -0,0 +1,5 @@
+class AddDescriptiontoRecipes < ActiveRecord::Migration[5.0]
+  def change
+    add_column :recipes, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161203000402) do
+ActiveRecord::Schema.define(version: 20161203001003) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20161203000402) do
     t.integer  "course_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "users", force: :cascade do |t|
