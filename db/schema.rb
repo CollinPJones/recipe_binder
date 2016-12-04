a0c86d3259464beabd7bc8b42a1a60e068a5ba48
Collin  Jones <collinpjones@users.noreply.github.com>
2016-12-03 21:01:49 -0600

    Created and migrated Bookmark DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204023619) do
+ActiveRecord::Schema.define(version: 20161204025948) do
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
