9c8bd655566e33f8d31c6562aecef7616c194657
Collin  Jones <collinpjones@users.noreply.github.com>
2016-12-03 20:27:50 -0600

    Added seedsrb script and CVS files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text_unit = File.read(Rails.root.join('lib', 'seeds', 'measurement_units.csv'))
+csv_unit = CSV.parse(csv_text_unit, :headers => true, :encoding => 'ISO-8859-1')
+csv_unit.each do |row|
+  u = MeasurementUnit.new
+  u.name = row ['name'].to_s
+  u.measurement_id = Measurement.find_by( name: row['measurement']).id
+  u.save
+  puts "#{u.name} unit for #{u.measurement.name} saved"
+end
+
+puts "There are now #{MeasurementUnit.count} rows in the measurement units table"
+
+
+csv_text_ingredient = File.read(Rails.root.join('lib', 'seeds', 'ingredients.csv'))
+csv_ingredient = CSV.parse(csv_text_ingredient, :headers => true, :encoding => 'ISO-8859-1')
+csv_ingredient.each do |row|
+  i = Ingredient.new
+  i.name = row['name'].to_s
+  i.purchase_container = row['purchase_container'].to_s
+  i.purchase_quantity = row['purchase_quantity'].to_f
+  i.category_id = IngredientCategory.find_by( name: row['category']).id
+  i.purchase_measurement_unit_id = MeasurementUnit.find_by( name: row['measurement_unit']).id
+  i.save
+  puts "#{i.name} saved"
+end
+
+puts "There are now #{Ingredient.count} rows in the ingredients table"
