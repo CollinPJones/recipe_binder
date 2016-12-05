a32517b2132488c81a0f260b19ce24bbcd12159d
Collin  Jones <collinpjones@users.noreply.github.com>
2016-12-05 17:46:31 -0600

    added image column to Course DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204025948) do
+ActiveRecord::Schema.define(version: 20161205233406) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20161204025948) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "directions", force: :cascade do |t|
