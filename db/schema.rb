2240c809dba8c7e113b229de6b4210f75040b87f
Collin  Jones <collinpjones@users.noreply.github.com>
2016-12-05 17:54:44 -0600

    added image column to Recipes DB and generated Image Uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205233406) do
+ActiveRecord::Schema.define(version: 20161205234705) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20161205233406) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "description"
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
